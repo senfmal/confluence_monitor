8413be40b78d518e5b69d152f8b7682e3882cae1
Veith <EXT-Veith@helaba-invest.de>
2019-05-02 15:34:10 +0200

    Added "table" with three tkinter.Listbox widgets as columns, bound together on scrollbar and mousewheel event level

--- main.py
+++ main.py
@@ -10,27 +10,72 @@ class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
-        self.pack()
+        self.grid()
         self.create_widgets()
 
 
     def create_widgets(self):
         self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Refresh data\n(click me)"
+        self.hi_there["text"] = "Refresh data"
         self.hi_there["command"] = self.display_conf_update_info
-        self.hi_there.pack(side="top")
+        self.hi_there.grid(row=0, column=0, sticky=tk.W)
 
         self.quit = tk.Button(self, text="QUIT", fg="red", command=self.master.destroy)
-        self.quit.pack(side="bottom")
+        self.quit.grid(row=0, column=3, sticky=tk.E)
+
+        tk.Frame(self, height=5, bd=1, relief=tk.SUNKEN).grid(row=2, columnspan=4, sticky=tk.E+tk.W)
+
+        self.labelColA = tk.Label(self, text="Page", background='lightblue')
+        self.labelColB = tk.Label(self, text="Type", background='lightblue')
+        self.labelColC = tk.Label(self, text="Days w/o update", background='lightblue')
+        self.labelColA.grid(row=3, column=0, sticky=tk.E+tk.W)
+        self.labelColB.grid(row=3, column=1, sticky=tk.E+tk.W)
+        self.labelColC.grid(row=3, column=2, sticky=tk.E+tk.W)
+
+        self.vsb = tk.Scrollbar(self, orient="vertical", command=self.OnVsb)
+        self.colA = tk.Listbox(self, yscrollcommand=self.vsb.set)
+        self.colB = tk.Listbox(self, yscrollcommand=self.vsb.set)
+        self.colC = tk.Listbox(self, yscrollcommand=self.vsb.set)
+
+        self.vsb.grid(column=3, sticky=tk.N+tk.S)
+        self.colA.grid(row=4, column=0)
+        self.colB.grid(row=4, column=1)
+        self.colC.grid(row=4, column=2)
+
+        self.colA.bind("<MouseWheel>", self.OnMouseWheel)
+        self.colB.bind("<MouseWheel>", self.OnMouseWheel)
+        self.colC.bind("<MouseWheel>", self.OnMouseWheel)
+
+
+    def OnVsb(self, *args):
+        self.colA.yview(*args)
+        self.colB.yview(*args)
+        self.colC.yview(*args)
+
+    def OnMouseWheel(self, event):
+        self.colA.yview("scroll", event.delta*-1, "units")
+        self.colB.yview("scroll", event.delta*-1, "units")
+        self.colC.yview("scroll", event.delta*-1, "units")
+        return "break"
 
 
     def display_conf_update_info(self):
         update_info = get_conf_update_information(acquire_conf_connection(url), space, theme)
-        self.table = st.SimpleTable(self, len(update_info)+1, 3)
-        self.table.pack(fill="x")
-        self.table.set(0,0,"Page")
-        self.table.set(0,1, "Type")
-        self.table.set(0,2, "Days w/o update")        
+        for k, page in update_info.items():
+            self.colA.insert("end", page['name'])
+            type = ''
+            if 'is_block' in page.keys():
+                type = 'Block'
+            if 'is_vorhaben' in page.keys():
+                type = 'Vorhaben'
+            if 'is_status' in page.keys():
+                type = 'Status'
+            self.colB.insert("end", type)
+            self.colC.insert("end", page['lastUpdated'])
+            self.colA.config(width=0)
+            self.colB.config(width=0)
+            self.colC.config(width=15)
+            self.winfo_toplevel().wm_geometry("")
 
 
 if __name__ == '__main__':
@@ -41,5 +86,6 @@ if __name__ == '__main__':
         sys.exit()
 
     root = tk.Tk()
+    root.title("Confluence Monitor v0.1")
     app = Application(master=root)
     app.mainloop()
