c17aed1e5ec6c8f67bfe0fe92d7b68c8af1b113a
Veith <EXT-Veith@helaba-invest.de>
2019-05-23 11:37:57 +0200

    removed deprecated method

--- confluence/ConfLastUpdated.py
+++ confluence/ConfLastUpdated.py
@@ -31,47 +31,6 @@ def get_conf_pages_ids(confluence, space, start=0, limit=500):
     ] if pages is not None else []
 
 
-def depr_get_conf_update_information(confluence, space, theme):
-    update_stats = {}
-    for page in get_conf_pages_ids(confluence, space):
-        if confluence.page_exists(space, page[1]):
-            labels = confluence.get_page_labels(
-                page[0], prefix=None, start=None, limit=None
-            )
-            vals = {}
-            vals['name'] = page[1]
-            for label in labels['results']:
-                if label['name'] == theme:
-                    vals['lastUpdated'] = (datetime.now() - datetime.strptime(
-                        get_information_from_content(
-                            confluence.get_page_by_id(
-                                page[0],
-                                expand='version'
-                            ),
-                            'version',
-                            'when'
-                        ),
-                        '%Y-%m-%dT%H:%M:%S.%f%z'
-                        ).replace(tzinfo=None)).days
-                if label['name'] == "block":
-                    vals['is_block'] = True
-                if label['name'] in (
-                    "vorhaben", "projekt", "maßnahme", "project", "massnahme"
-                ):
-                    vals['is_vorhaben'] = True
-                if label['name'] in (
-                    "status", "statusbericht", "bericht", "statusreport", "report"
-                ):
-                    vals['is_status'] = True
-                if label['name'] in (
-                    "news", "neuigkeit", "update", "kommunikation"
-                ):
-                    vals['is_news'] = True
-            if 'lastUpdated' in vals:
-                update_stats[page[0]] = vals
-    return update_stats
-
-
 def get_conf_update_information(confluence, space, theme):
     names = []
     lastUpdates = []
